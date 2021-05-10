1804d9be537b1de3d550e44ea3f8ac0fe04cc677
asaf.arviv@gmail.com <asaf.arviv@gmail.com>
2021-05-10 11:16:42 +0300

    first time login load handle

--- src/main/java/com/servlets/MainServlet.java
+++ src/main/java/com/servlets/MainServlet.java
@@ -6,6 +6,8 @@ import com.beans.User;
 import com.beans.WorkingHour;
 import com.google.gson.Gson;
 import com.services.ReportingServices;
+import org.apache.log4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,10 +19,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
-import org.apache.log4j.Logger;
 
 @WebServlet(name = "mainServlet", value = "/mainServlet")
-public class MainServlet  extends HttpServlet {
+public class MainServlet extends HttpServlet {
 
     static Logger logger = null;
 
@@ -46,7 +47,6 @@ public class MainServlet  extends HttpServlet {
     }
 
 
-
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         doGet(request, response);
@@ -69,24 +69,22 @@ public class MainServlet  extends HttpServlet {
                 UsersDisplay(request, response);
             } else if ("loadUsers".equals(action)) {
                 UsersJSONDisplay(request, response);
-            } else if ( "enter".equals(action) || "exit".equals(action)) {
+            } else if ("enter".equals(action) || "exit".equals(action)) {
                 cardStamping(request, response);
             } else if ("view".equals(action)) {
                 viewAllReports(request, response);
             } else {
                 Login(request, response);
             }
-        } catch ( SQLException | ServletException e) {
+        } catch (SQLException | ServletException e) {
             e.printStackTrace();
         }
     }
 
 
-
-
     private void cardStamping(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException, SQLException {
-        logger.info("MainServlet.cardStamping : " );
+        logger.info("MainServlet.cardStamping : ");
 
         HttpSession session = request.getSession();
 
@@ -94,7 +92,7 @@ public class MainServlet  extends HttpServlet {
         PrintWriter out = response.getWriter();
         User user = (User) session.getAttribute("currentUser");
         String action = request.getParameter("act");
-        boolean isCardStampOK = ReportingServices.cardStamping(user,action);
+        boolean isCardStampOK = ReportingServices.cardStamping(user, action);
         List<WorkingHour> list = ReportingServices.fetchAllWorkingHours();
         Gson gson = new Gson();
         String listAsJSON = gson.toJson(list);
@@ -107,7 +105,7 @@ public class MainServlet  extends HttpServlet {
 
     private void viewAllReports(HttpServletRequest request, HttpServletResponse response) throws SQLException {
         try {
-            logger.info("MainServlet.report : " );
+            logger.info("MainServlet.report : ");
             response.setContentType("text/plain");
             PrintWriter out = response.getWriter();
             List<WorkingHour> list = ReportingServices.fetchAllWorkingHours();
@@ -127,7 +125,7 @@ public class MainServlet  extends HttpServlet {
 
 
     private void DeleteUser(HttpServletRequest request, HttpServletResponse response) {
-        logger.info("MainServlet.DeleteUser : " );
+        logger.info("MainServlet.DeleteUser : ");
         try {
             String userId = request.getParameter("id");
             ReportingServices.deleteUser(userId);
@@ -138,10 +136,8 @@ public class MainServlet  extends HttpServlet {
     }
 
 
-
-
     private void Register(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-        logger.info("MainServlet.Register : " );
+        logger.info("MainServlet.Register : ");
         String id = request.getParameter("identityNo");
         String fullname = (String) request.getParameter("fullName");
         String email = (String) request.getParameter("email");
@@ -157,7 +153,7 @@ public class MainServlet  extends HttpServlet {
 
     private void UsersJSONDisplay(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, ServletException, IOException {
-        logger.info("MainServlet.UsersJSONDisplay : " );
+        logger.info("MainServlet.UsersJSONDisplay : ");
         response.setContentType("text/plain");
         PrintWriter out = response.getWriter();
         List<User> list = ReportingServices.fetchAllUsers();
@@ -171,23 +167,26 @@ public class MainServlet  extends HttpServlet {
 
     private void UsersDisplay(HttpServletRequest request, HttpServletResponse response) throws SQLException {
         try {
-            logger.info("MainServlet.UsersDisplay : " );
+            logger.info("MainServlet.UsersDisplay : ");
             List<User> list = ReportingServices.fetchAllUsers();
             request.setAttribute("list", list);
             String destPage = "users.jsp";
             RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
             dispatcher.forward(request, response);
         } catch (Exception e) {
-            logger.error(e.getMessage()); e.printStackTrace();
+            logger.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
 
     private void Login(HttpServletRequest request, HttpServletResponse response) {
-        logger.info("MainServlet.Login : " );
+        logger.info("MainServlet.Login : ");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        // String fullName = request.getParameter("fullName");
+        if (email == null && password == null) {
+
+        }
         LoginDao loginDao = new LoginDao();
         User user = new User();
         user.setPassword(password);
@@ -195,23 +194,26 @@ public class MainServlet  extends HttpServlet {
         try {
             User user1 = loginDao.validateLogin(user);
             String destPage = "login.jsp";
-            if (user1 != null) {
-                HttpSession session = request.getSession();
-                session.setAttribute("currentUser", user1);
-                request.setAttribute("login_msg", "congradulations !!");
-                destPage = "users.jsp";
-                List<User> list = ReportingServices.fetchAllUsers();
-                request.setAttribute("list", list);
-            } else {
-
-                String message = "Invalid email/password";
-                request.setAttribute("login_msg", message);
-                logger.error("MainServlet.Login Login failed !!! with email: " + email);
+            if (email != null && password != null) {
+                if (user1 != null) {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("currentUser", user1);
+                    request.setAttribute("login_msg", "congradulations !!");
+                    destPage = "users.jsp";
+                    List<User> list = ReportingServices.fetchAllUsers();
+                    request.setAttribute("list", list);
+                } else {
+                    String message = "Invalid email/password";
+                    request.setAttribute("login_msg", message);
+                    logger.error("MainServlet.Login Login failed !!! with email: " + email);
+                }
             }
+
             RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
             dispatcher.forward(request, response);
         } catch (Exception e) {
-            logger.error(e.getMessage()); e.printStackTrace();
+            logger.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
