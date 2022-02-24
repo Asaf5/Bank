45b06568899156c2d21195062f3b92037e6eaa55
asaf-blooma <asafarviv@blooma.ai>
2022-02-24 14:01:29 +0200

    sdsadadd

--- src/main/java/com/beans/User.java
+++ src/main/java/com/beans/User.java
@@ -7,7 +7,7 @@ public class User {
     private String password;
     private String fullName;
     private String fdsffsf;
-    private String fdsfds;
+    private String VVV;
 
     public User() {
     }
